66ca7469e3aefd573a38621d527710570fcd4509
ATOUNSI1996 <tounsi.ahmed96@gmail.com>
2022-07-23 12:51:48 +0100

    constructeurs

--- complexe.cpp
+++ complexe.cpp
@@ -6,15 +6,27 @@ class Cmplx
     public:
     float re;
     float im;
+
+    //definition du contructeurs
+    Cmplx()
+    {
+        cout << "je suis dans le constructeurs.....";
+    } 
+    Cmplx(float a, float b)
+    {
+        cout << "je suis dans le deuxieme constructeurs.....";
+        re = a;
+        im = b;
+    }
     //definition des méthodes
-    // Cmplx addition(Cmplx c)
-    // {
-    //     Cmplx res;
-    //     res.re = (c.re + c2.re);
-    //     res.im = (c1.im + c2.im);
-    //     cout << "res = " << res.re << "+i" << res.im;
-    //     return res;
-    // }
+    Cmplx addition(Cmplx c)
+    {
+        Cmplx res;
+        res.re = (c.re + re);
+        res.im = (c.im + im);
+        cout << "res = " << res.re << "+i" << res.im;
+        return res;
+    }
     void afficher()
     {
         cout << re << "+i" << im << endl;
@@ -26,11 +38,11 @@ int main()
     Cmplx z1;
     z1.re = 2;
     z1.im = 3;
-    // Cmplx z2;
+    Cmplx z2(4, 7);
     // z2.re = 2;
     // z2.im = 3;
-    // Cmplx z3 = addition( z1,  z2);
-    // cout << z3 << endl;
-    z1.afficher();
+    Cmplx z3 = z1.addition(z2);
+    z3.afficher();
+    // z1.afficher();
 
 }
