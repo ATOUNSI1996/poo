ac8735a53773a8043f91d7103a4ebdbae53af413
ATOUNSI1996 <tounsi.ahmed96@gmail.com>
2022-07-23 12:38:56 +0100

    affichage_cmplx

--- complexe.cpp
+++ complexe.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
-class cmplx
+class Cmplx
 {
+    //declaration des attribut
+    public:
     float re;
     float im;
-    int addition(cmplx c1, cmplx c2)
+    //definition des méthodes
+    // Cmplx addition(Cmplx c)
+    // {
+    //     Cmplx res;
+    //     res.re = (c.re + c2.re);
+    //     res.im = (c1.im + c2.im);
+    //     cout << "res = " << res.re << "+i" << res.im;
+    //     return res;
+    // }
+    void afficher()
     {
-        cmplx res;
-        res.re = (c1.re + c2.re);
-        res.im = (c1.im + c2.im);
-        cout << "res = " << res.re << "+i" << res.im;
+        cout << re << "+i" << im << endl;
     }
 };
+
+int main()
+{
+    Cmplx z1;
+    z1.re = 2;
+    z1.im = 3;
+    // Cmplx z2;
+    // z2.re = 2;
+    // z2.im = 3;
+    // Cmplx z3 = addition( z1,  z2);
+    // cout << z3 << endl;
+    z1.afficher();
+
+}
